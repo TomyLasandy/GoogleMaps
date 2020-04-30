0f44b33627ebfb7d5b7719133f4b7d2a17245e2a
Putraagam <64558982+Putraagam@users.noreply.github.com>
2020-04-30 14:53:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.googlemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+
+        LatLng Jambi = new LatLng(-2.494266, 102.713441);
+        map.addMarker(new MarkerOptions().position(Jambi).title("RM SATE KAMBING JEPARA SINGKUT BU HARTIK"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Jambi));
+    }
+}
